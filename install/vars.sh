2197a0601a3158d7a34358b68f2e3e348141c2a4
bigflood <pyotaesu@gmail.com>
2022-11-05 21:33:31 +0900

    upgrade kserve version: 0.8 -> 0.9

--- install/vars.sh
+++ install/vars.sh
@@ -3,11 +3,12 @@
 set -eu -o pipefail
 INSTALL_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-export K8S_VER=1.22.9
-export ISTIO_VERSION_M=1.11
-export ISTIO_VERSION=$ISTIO_VERSION_M.8
-export KNATIVE_VERSION=knative-v1.0.0
-export KSERVE_VERSION=v0.8.0
+export KIND_VER=0.17.0
+export K8S_VER=1.24.7
+export ISTIO_VERSION_M=1.14
+export ISTIO_VERSION=$ISTIO_VERSION_M.0
+export KNATIVE_VERSION=knative-v1.4.0
+export KSERVE_VERSION=v0.9.0
 export CERT_MANAGER_VERSION=v1.3.0
 export KUBECONFIG="$INSTALL_DIR/../.kubeconfig"
 export BIN_DIR="$INSTALL_DIR/bin"
