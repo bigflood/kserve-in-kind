acf44aafa9f9d190b3dc58d2c57ac0b2c001c486
bigflood <pyotaesu@gmail.com>
2022-11-06 00:27:29 +0900

    make m1 macos compatible

--- install/download-commands.sh
+++ install/download-commands.sh
@@ -37,6 +37,8 @@ OS2=$(echo "$OS" | sed -e 's/darwin/osx/')
 mkdir -p "$BIN_DIR"
 cd "$BIN_DIR"
 
-download_bin "https://kind.sigs.k8s.io/dl/v$KIND_VER/kind-$OS-$ARCH" "$KIND_CLI"
+# NOTE: istio is not arm compatiable.
+download_bin "https://kind.sigs.k8s.io/dl/v$KIND_VER/kind-$OS-amd64" "$KIND_CLI"
+
 download_tgz_bin "https://github.com/istio/istio/releases/download/$ISTIO_VERSION/istioctl-$ISTIO_VERSION-$OS2-$ARCH.tar.gz" "istioctl" "$ISTIO_CLI"
 download_bin "https://dl.k8s.io/release/v$K8S_VER/bin/$OS/$ARCH/kubectl" "$KUBECTL_CLI"
